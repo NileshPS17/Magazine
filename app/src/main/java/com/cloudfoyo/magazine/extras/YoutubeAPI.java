f4b373de50b6b3ec2e780d9bc454688822e5cda7
NileshPS17 <nps17thatsme@gmail.com>
2016-01-03 21:46:52 +0530

    Added Video playback Support

--- app/src/main/java/com/cloudfoyo/magazine/extras/YoutubeAPI.java
+++
@@ -1,9 +0,0 @@
-package com.cloudfoyo.magazine.extras;
-
-/**
- * Created by nilesh on 3/1/16.
- */
-public class YoutubeAPI {
-
-    public static final String DEV_KEY = "AIzaSyA2RaKp6c0bZWCtXTNTMIm_1qUHXQlnoJQ";
-}
